e9d7dfce08e4e195f703ef535047e0c943d73a65
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-21 12:23:32 +0530

    db conn

---
+++ app/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+data_base_url="mysql+pymysql://root:harsha%40345@localhost/task_manager"
+
+engine=create_engine(data_base_url)
+
+SessionLocal=sessionmaker(
+    autocomit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base=declarative_base
