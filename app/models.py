098a59428c46981e43de583de63738fb7b05f17a
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-21 12:31:24 +0530

    table model py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from sqlalchemy import Column,Integer,String
 from database import Base
 
+class Usercreated(Base):
+    __tablename__="users"
+
+    id=Column(Integer,primary_key=True,index=True)
+    username=Column(String(80))
+    email=Column(String(100),unique=True)
+    password=Column(String)
+    
