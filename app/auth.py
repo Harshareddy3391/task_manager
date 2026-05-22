d8954ecfee49ef47d0574754004957c1f7d3c054
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-22 16:18:19 +0530

    create jwt in auth

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,6 @@
 from passlib.context import CryptContext
+from jose import JWTError,jwt
+from datetime import datetime,timedelta
 
 
 
@@ -6,6 +8,11 @@ pwd_context=CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
+
+
+SECRET_KEY="mysecreatkey"
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES=30
 
 
 def hash_password(password:str):
@@ -23,5 +30,31 @@ def verify_password(plain_password,hash_password):
         plain_password,
         hash_password
     )
+
+
+def create_access_token(data:dict):
+
+    to_encode=data.copy()
+
+    expire=datetime.utcnow()+timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+
+    to_encode.update({
+        "exp":expire
+        }
+    )
+
+
+    encoded_jwt=jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
+
+
 
 
